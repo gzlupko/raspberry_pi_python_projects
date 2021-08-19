d416c01a04c47498377b7e3f5529fc121a3fcb58
gzlupko <gianzlupko@gmail.com>
2021-08-19 16:54:08 -0400

    added code for simple motion sensor attachment

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-# test git connect
+# test py installations
 
- import pandas as pd
+from gpiorzero import MotionSensor, LED
+from signal import pause
+
+pir = MotionSenor(4)
+led = LED(16)
+
+pir.when_motion = led.on
+pir.when_no_motion = led.off
+
+pause() 
  
